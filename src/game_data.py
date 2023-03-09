e7e2c64a116502c3ff2665064c65bf61d4933cdb
Alberto Guijarro Rodriguez <albertoportuk16@gmail.com>
2023-03-09 08:41:27 +0000

    imported terrain

---
+++ src/game_data.py
@@ -0,0 +1,15 @@
+level_0 = {
+    'bg_palm':      'levels/0/level_0._bg_palm.csv',
+    'coin':         'levels/0/level_0._coin.csv',
+    'constraints':  'levels/0/level_0._constraints.csv',
+    'crates':       'levels/0/level_0._crates.csv',
+    'enemies':      'levels/0/level_0._enemies.csv',
+    'fg_palms':     'levels/0/level_0._fg_palms.csv',
+    'grass':        'levels/0/level_0._grass.csv',
+    'player':       'levels/0/level_0._player.csv',
+    'terrain':      'levels/0/level_0._terrain.csv'
+}
+
+
+
+
---
+++ src/level.py
@@ -0,0 +1,40 @@
+import pygame
+
+from support import import_csv_layout, import_cut_graphics
+from settings import tile_size
+from tiles import Tile, StaticTile
+
+class Level:
+    def __init__(self, level_data, surface):
+        # general setup
+        self.display_surface = surface
+        self.world_shift = -5
+
+        # terrain setup
+        terrain_layout = import_csv_layout(level_data.get('terrain'))
+        self.terrain_sprites = self.create_tile_group(terrain_layout, 'terrain')
+        
+        pass
+    
+    def create_tile_group(self, layout, type) -> pygame.sprite.Group:
+        sprite_group = pygame.sprite.Group()
+
+        for row_index, row in enumerate(layout):
+            for col_index, val in enumerate(row):
+                if val != '-1':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+
+                    if type == 'terrain':
+                        terrain_tile_list = import_cut_graphics(r'graphics\terrain\terrain_tiles.png')
+                        tile_surface = terrain_tile_list[int(val)]
+                        sprite = StaticTile(tile_size, x, y, tile_surface)
+                        sprite_group.add(sprite)
+
+        return sprite_group
+    
+    def run(self):
+        # run entire game/level
+        self.terrain_sprites.draw(self.display_surface)
+        self.terrain_sprites.update(self.world_shift)
+        pass
